39fa76c9a41cdb10026d23c7f066bcbbf572a846
Servaturus <servaturus@gmx.net>
2022-01-21 21:54:39 +0100

    Added a basic listener class
    A Listener class with an example eventlistener-method in it.

---
+++ src/main/java/woolbattle/woolbattle/Listener.java
@@ -0,0 +1,11 @@
+package woolbattle.woolbattle;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class Listener implements org.bukkit.event.Listener {
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+    }
+
+}
