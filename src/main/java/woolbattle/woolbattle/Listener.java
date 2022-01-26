3f9c3aa65460b7ddaf6f4a2ce69215326d5523ea
Servaturus <servaturus@gmx.net>
2022-01-26 20:21:13 +0100

    Enhancing the functionality of the blockBreakListener method.

---
+++ src/main/java/woolbattle/woolbattle/Listener.java
@@ -0,0 +1,59 @@
+package woolbattle.woolbattle;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Listener implements org.bukkit.event.Listener {
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+
+        boolean blockIsMap = true; //Is going to be changed, to actually check, whether the block broken is part of the map
+        ItemStack itemStack = new ItemStack(){};
+        int amount = 1; //Is to be changed, to be modifiable by the user
+        int itemAmount = 0;
+        short dura = 238;
+        if(blockIsMap){
+
+            Material type = event.getBlock().getType();
+
+            if(type.equals(Material.WOOL)){
+                event.setCancelled(true);
+                /*if(event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.SHEARS)){
+                    event.getPlayer().getInventory().getItemInMainHand().setDurability((short) 238);
+                }else{}*/
+
+
+
+                for(ItemStack is : event.getPlayer().getInventory()){
+                    if(is != null){
+                        itemAmount = is.getType().equals(Material.WOOL)? itemAmount+is.getAmount() : itemAmount;
+                        if(is.getType().equals(Material.SHEARS)){
+                            is.setDurability(is.getType().getMaxDurability());
+                        }
+                    }
+                }
+
+                itemStack.setType(type); // The wool, additionally to this, is to be coloured according to the team colour of the player, breaking the block
+                if(itemAmount < 192){
+                    itemStack.setAmount(amount);
+                    event.getPlayer().getInventory().addItem(itemStack);
+                }else{}
+
+                event.getBlock().setType(Material.AIR);
+
+
+                new BukkitRunnable(){
+                @Override
+                public void run() {
+                    event.getBlock().setType(type);
+                }
+                }.runTaskLater(Main.getInstance(), 10);
+
+            }
+        }
+    }
+}
--- src/main/java/woolbattle/woolbattle/Main.java
+++ src/main/java/woolbattle/woolbattle/Main.java
@@ -1,17 +1,24 @@
 package woolbattle.woolbattle;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+    private static Main instance;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new Listener(), this);
+        instance = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        
     }
+    public static Main getInstance(){
+        return instance;
+    }
+
 }
