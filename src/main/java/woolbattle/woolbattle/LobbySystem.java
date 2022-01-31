73f35d0cd97cd671c3d4374c25ff773685693936
Niklas <1234niklasmoeller@gmx.at>
2022-01-31 22:50:05 +0100

    Implemented basic LobbySystem
    Scoreboard & Cooldown Ready

--- src/main/java/woolbattle/woolbattle/LobbySystem.java
+++ src/main/java/woolbattle/woolbattle/LobbySystem.java
@@ -1,20 +1,197 @@
 package woolbattle.woolbattle;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.*;
 
 import java.util.Collection;
 
-public class LobbySystem {
+public class LobbySystem implements Listener {
 
-    private static boolean gameStarted = false;
+    static boolean gameStarted = false;
+    static boolean runCooldownTask = false;
+    static boolean runScoreBoardTask = false;
+    static int cooldown = 60;
 
-    public static boolean isAlreadyStarted(){
-        return gameStarted;
+    static String dummyTeam = "Rot";
+    static String dummyMap = "Default";
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(ChatColor.GRAY + "Der Spieler " + ChatColor.GREEN + player.getDisplayName()
+                + ChatColor.GRAY + " hat das Spiel verlassen.");
+        //TODO: implement Logic if a player who is in a game leaves
+        if(Bukkit.getServer().getOnlinePlayers().size() >= 1){
+            endGame();
+        }
     }
 
-    public static void setXPBar() {
-        Collection<Player> onlinePlayers = Bukkit.getOnlinePlayers();
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        event.setJoinMessage(ChatColor.GRAY + "Der Spieler " + ChatColor.GREEN + player.getDisplayName()
+                + ChatColor.GRAY + " ist dem Spiel beigetreten.");
+
+        if(gameStarted){
+            player.setGameMode(GameMode.SPECTATOR);
+            player.sendMessage(ChatColor.RED + "Es läuft bereits ein Spiel!");
+        }
+
+        setPlayerScoreBoard(player);
+
+        if(!runCooldownTask){
+            updatePlayerCooldown();
+        }
+
+        if(!runScoreBoardTask){
+            updateScoreBoard();
+        }
+
+    }
+
+    public static void startGame(){
+        //TODO: basic whole functions
+        gameStarted = true;
+        Bukkit.broadcastMessage(ChatColor.GREEN + "Game Starting...");
+    }
+
+    public static void endGame(){
+        //TODO: basic whole functions
+        gameStarted = false;
+        Bukkit.broadcastMessage(ChatColor.RED + "Game Ending...");
+    }
+
+
+    public static void updatePlayerCooldown(){
+        runCooldownTask = true;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(!gameStarted){
+                    Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+                    int playerAmount = players.size();
+                    int maxPlayers = Bukkit.getServer().getMaxPlayers();
+                    if(playerAmount >= (maxPlayers / 2)){
+                        if(cooldown == 0){
+                            startGame();
+                        }
+                        else if(cooldown > 15 && playerAmount >= ((maxPlayers / 4) * 3)){
+                            cooldown = 15;
+                        }
+                        cooldown -= 1;
+                    }
+                    else{
+                        cooldown = 60;
+                    }
+                    for(Player player: players){
+                        if(player.getGameMode() != GameMode.SPECTATOR) {
+                            setPlayerCooldown(player, cooldown);
+                        }
+                        else{
+                            System.out.println("spec detected");
+                        }
+
+                    }
+
+                }
+            }
+        }.runTaskTimer(Main.getInstance(), 0, 20);
+
+    }
+
+
+
+    public static void setPlayerCooldown(Player player, int level) {
+        float exp = (float) level/60;
+        player.setExp(exp);
+        player.setLevel(level);
+
+    }
+
+    public void updateScoreBoard(){
+        runScoreBoardTask = true;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                //TODO: Check if player is participating in the game NOT if any game running
+                if(!gameStarted){
+                    Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
+
+                    for(Player player : players){
+                        updatePlayerScoreBoard(player);
+
+                    }
+                }
+
+            }
+        }.runTaskTimer(Main.getInstance(), 0, 20);
+
+    }
+
+    public static void setPlayerScoreBoard(Player player){
+        int maxPlayers = Bukkit.getServer().getMaxPlayers();
+        int actualPlayers = Bukkit.getServer().getOnlinePlayers().size();
+
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective obj = board.registerNewObjective("Woolbattle", "dummy");
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.setDisplayName("§aWoolbattle Lobby");
+
+        Team team = board.registerNewTeam("team");
+        Team map = board.registerNewTeam("map");
+        Team players = board.registerNewTeam("players");
+
+        obj.getScore("\u1CBC\u1CBC\u1CBC").setScore(8);
+
+        obj.getScore("§7»Team").setScore(7);
+        obj.getScore("§c").setScore(6);
+
+        obj.getScore("\u1CBC\u1CBC").setScore(5);
+
+        obj.getScore("§7»Map").setScore(4);
+        obj.getScore("§d").setScore(3);
+
+        obj.getScore("\u1CBC").setScore(2);
+
+        obj.getScore("§7»Spieler").setScore(1);
+        obj.getScore("§b").setScore(0);
+
+        team.addEntry("§c");
+        team.setPrefix("§c" + dummyTeam);
+
+        map.addEntry("§d");
+        map.setPrefix("§d" + dummyMap);
+
+        players.addEntry("§b");
+        players.setPrefix("§b" + actualPlayers + "/" + maxPlayers);
+
+        player.setScoreboard(board);
+
+    }
+
+    public void updatePlayerScoreBoard(Player player){
+        int maxPlayers = Bukkit.getServer().getMaxPlayers();
+        int actualPlayers = Bukkit.getServer().getOnlinePlayers().size();
+        Scoreboard board = player.getScoreboard();
+
+        Team team = board.getTeam("team");
+        Team map = board.getTeam("map");
+        Team players = board.getTeam("players");
+
+        team.setPrefix("§c" + dummyTeam);
+        map.setPrefix("§d" + dummyMap);
+        players.setPrefix("§b" + actualPlayers + "/" + maxPlayers);
+
 
     }
 
--- src/main/java/woolbattle/woolbattle/Main.java
+++ src/main/java/woolbattle/woolbattle/Main.java
@@ -5,14 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main instance;
+
     @Override
     public void onEnable() {
         // Plugin startup login
-        Bukkit.getPluginManager().registerEvents(new Listener(), this);
+        Bukkit.getPluginManager().registerEvents(new LobbySystem(), this);
+        instance = this;
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static Main getInstance(){
+        return instance;
+    }
+
 }
