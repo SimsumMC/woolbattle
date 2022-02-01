2442ac077425e187b5dddd555b8710df8f6736d1
BeelzebubGYRI <jakob@almanbot.de>
2022-02-01 15:36:19 +0100

    Format update
    Updated format so that the code is more easily readable

--- src/main/java/woolbattle/woolbattle/Main.java
+++ src/main/java/woolbattle/woolbattle/Main.java
@@ -1,13 +1,13 @@
 package woolbattle.woolbattle;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new TeamSystem(), this);
     }
 
     @Override
--- src/main/java/woolbattle/woolbattle/TeamSystem.java
+++ src/main/java/woolbattle/woolbattle/TeamSystem.java
@@ -1,28 +1,95 @@
 package woolbattle.woolbattle;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
-import javax.swing.text.html.parser.Entity;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.Objects;
 
-public class TeamSystem {
-    public TeamSystem(EntityDamageByEntityEvent event){
-        String DamagingPlayer = String.valueOf(event.getDamager());
-        String DamagedPlayer = String.valueOf(event.getEntity());
+public class TeamSystem implements Listener, CommandExecutor {
 
-        HashMap<Integer, String> players = new HashMap<Integer, String>();
-        players.put(1, "randPlayer1");
-        players.put(2, "randPlayer2");
-        players.put(3, "randPlayer3");
-        players.put(4, "randPlayer4");
+    public int teamLimit = 2;
+    public ArrayList<HumanEntity> membersRed = new ArrayList<>();
 
-        for(int amPlayers = players.size(); amPlayers > 0; amPlayers--){
-            if(players.get(amPlayers) = ){
+    @EventHandler
+    public void clickEvent(InventoryClickEvent click){
+        if (click.getCurrentItem() == null || !Objects.equals(click.getCurrentItem().getItemMeta().getDisplayName(), ChatColor.RED + "Vote for team Red")) return;
+        else{
+            if (click.getView().getTitle().equals(ChatColor.RED + "Team Voting")) {
+                HumanEntity player1 = click.getWhoClicked();
+                if (membersRed.contains(click.getWhoClicked())) {
+                    click.setCancelled(true);
+                    click.getWhoClicked().sendMessage(ChatColor.DARK_RED + "You have already entered this Team!");
+                    click.setCancelled(true);
+                } else {
+                    if (membersRed.size() >= teamLimit) {
+                        click.getWhoClicked().sendMessage(ChatColor.RED + "The Team already has " + teamLimit + " Members.");
 
+                    } else {
+                        membersRed.add(player1);
+                        click.getWhoClicked().sendMessage(String.valueOf(membersRed));
+                        click.getWhoClicked().sendMessage(ChatColor.RED + "You have entered Team Red");
+                    }
+                    click.setCancelled(true);
+                }
             }
         }
     }
+
+    @EventHandler
+    public void DamageProtection(EntityDamageByEntityEvent damage){
+        if (damage.getEntity() instanceof Player && damage.getDamager() instanceof Zombie) {
+            Vector velocity;
+            velocity = damage.getEntity().getVelocity();
+            damage.getEntity().setVelocity(velocity);
+            damage.setCancelled(true);
+        }
+        else return;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            Inventory voting = Bukkit.createInventory(null, 27, ChatColor.RED + "Team Voting");
+
+            // Adding the "Vote for red" item
+            ItemStack voteRed = new ItemStack(Material.WOOL, 1, (byte)14);
+            ArrayList<String> voteRedLore = new ArrayList<>();
+            ItemMeta voteRedMeta = voteRed.getItemMeta();
+
+            voteRedMeta.setDisplayName(ChatColor.RED + "Vote for team Red");
+            voteRedMeta.setLore(voteRedLore);
+            voteRed.setItemMeta(voteRedMeta);
+            voting.setItem(10, voteRed);
+
+            // Adding the "Vote for blue" item
+            ItemStack voteBlue = new ItemStack(Material.WOOL, 1, (byte)11);
+            ArrayList<String> voteBlueLore = new ArrayList<>();
+            ItemMeta voteBlueMeta = voteRed.getItemMeta();
+
+            voteBlueMeta.setDisplayName(ChatColor.RED + "Vote for team Red");
+            voteBlueMeta.setLore(voteBluedLore);
+            voteBlue.setItemMeta(voteRedMeta);
+            voting.setItem(10, voteRed);
+            player.openInventory(voting);
+        }
+
+        return true;
+    }
+
 }
