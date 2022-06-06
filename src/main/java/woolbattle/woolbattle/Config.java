b6c2436af44b467aebc5012b3470cafcfa3abb9f
Niklas <1234niklasmoeller@gmx.at>
2022-06-06 12:59:09 +0200

    Better default config (new values, now with directory)

--- src/main/java/woolbattle/woolbattle/Config.java
+++ src/main/java/woolbattle/woolbattle/Config.java
@@ -6,9 +6,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Config {
@@ -18,7 +22,13 @@ public class Config {
      * @author SimsumMC & Servaturus
      */
 
-    public static final String connectionString = (String) getValue("connectionString");
+    private static boolean fileExisting = false;
+
+    public static final String mongoDatabase = (String) getValue("mongodb");
+
+    static{
+        System.out.println(Config.mongoDatabase);
+    }
 
     public static final String defaultMap = (String) getValue("mapName");
     public static final int defaultLives = (int) (long) getValue("defaultLives");
@@ -92,18 +102,53 @@ public class Config {
      * @param key the JSON key as a String<br>
      * @author SimsumMC & Servaturus
      */
-    public static Object getValue(String key) {
+    public static  Object getValue(String key) {
+        if (!fileExisting) {
+
+            // make sure that the directory exists
+            File directory = new File("plugins/WoolBattle");
+            directory.mkdir();
+
+            File file = new File("plugins/WoolBattle/config.json");
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                    Files.write(Paths.get(file.toURI()), Collections.singleton("{\n" +
+                            "  \"mongodb\": \"ADD the connection string HERE\",\n" +
+                            "  \"mapName\": \"MapName\",\n" +
+                            "  \"mapSpawn\": [0, 0, 0],\n" +
+                            "  \"lobbySpawn\": [0, 0, 0],\n" +
+                            "  \"defaultLives\": 10,\n" +
+                            "  \"spawnProtectionAfterDeath\": 5,\n" +
+                            "  \"spawnProtectionAtGameStart\": 15,\n" +
+                            "  \"startCooldown\": 60,\n" +
+                            "  \"skipCooldown\": 30,\n" +
+                            "  \"deathCooldown\": 10,\n" +
+                            "  \"maxHeight\": 100,\n" +
+                            "  \"minHeight\": 0,\n" +
+                            "  \"teamSize\": 2,\n" +
+                            "  \"teamSpawns\": [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],\n" +
+                            "  \"givenWoolAmount\": 1,\n" +
+                            "  \"maxStacks\": 3,\n" +
+                            "  \"jumpCooldown\": 60,\n" +
+                            "  \"woolReplaceDelay\" : 10\n" +
+                            "}"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                fileExisting = true;
+            }
+        }
         JSONParser parser = new JSONParser();
         FileReader fileReader;
         JSONObject jsonObject = new JSONObject();
         try {
-            fileReader = new FileReader("plugins/config.json");
+            fileReader = new FileReader("plugins/WoolBattle/config.json");
             Object obj = parser.parse(fileReader);
             jsonObject = (JSONObject) obj;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
         return jsonObject.get(key);
-    }
-
+        }
 }
